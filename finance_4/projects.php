330adf0a58a5bd4bb972bbdd4b7c7aa6fb13a46b
my_new_git <boriskaborisenko@gmail.com>
2015-10-13 15:19:37 +0300

    responsive

---
+++ finance_4/projects.php
@@ -0,0 +1,204 @@
+            <div class="project">
+			  <div class="project_name">Regio Hessen</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.der-regio.de">der-regio.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Hellwegtaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.divib.de">divib.de</a></div>
+			</div>
+			
+			<!--<div class="project">
+			  <div class="project_name">Nahgold</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.nahgold.de">nahgold.de</a></div>
+			</div>-->
+			
+			<div class="project">
+			  <div class="project_name">Erzregio Gutscheine</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.erzregio.de">erzregio.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">ZschopauTaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.zschopautaler.info">zschopautaler.info</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">CARLO</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.carlo-regional.de">carlo-regional.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Elbtaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.elbtaler.de">elbtaler.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Regio Bayern</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.der-regio.de">der-regio.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">KannWas</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.kannwas.org">kannwas.org</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Gwinner</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.gwinner-ev.de">gwinner-ev.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Ammerlechtaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.ammerlechtaler.de">ammerlechtaler.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Freitaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.freitaler.com">freitaler.com</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Donautaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.donau-taler.de">donau-taler.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Sterntaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.regiostar.com">regiostar.com</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Hallertauer</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.hallertauer-regional.de">hallertauer-regional.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Bethel-Euro</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.bethel.de">bethel.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Chiemgauer</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.chiemgauer.info">chiemgauer.info</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Ampertaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.amper-taler.de">amper-taler.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Bergtaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.bergtaler.de">bergtaler.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">beta</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.betabank.de">betabank.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Brot€inheit</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.wiben.de">wiben.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">BürgerBlüte</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.buergerbluete.de">buergerbluete.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Donauwörther</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.donauwoerth.de">donauwoerth.de</a></div>
+			</div>
+			
+			<!--<div class="project">
+			  <div class="project_name">DreyEcker</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.dreyecker.de">dreyecker.de</a></div>
+			</div>-->
+			
+			<div class="project">
+			  <div class="project_name">Eder-Taler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.eder-taler.de">eder-taler.de</a></div>
+			</div>
+			
+			<!--<div class="project">
+			  <div class="project_name">Engelgeld</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.engelgeld.de">engelgeld.de</a></div>
+			</div>-->
+			
+			<!--<div class="project">
+			  <div class="project_name">Grabfelder</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.der-grabfelder.de">der-grabfelder.de</a></div>
+			</div>-->
+			
+			<!--<div class="project">
+			  <div class="project_name">Havelblüte</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.havelblueten.de">havelblueten.de</a></div>
+			</div>-->
+			
+			<div class="project">
+			  <div class="project_name">Hohenloher Franken</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.hohenloher-franken.de">hohenloher-franken.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Lausitzer</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.lausitzer.net">lausitzer.net</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Lindentaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.lindentaler.org">lindentaler.org</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Lunar</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.regio-lunar.org">regio-lunar.org</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Pauer</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.pauer-aachen.de">pauer-aachen.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">RegioMark RheinMosel</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.regiovereinkoblenz.de">regiovereinkoblenz.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">RemsTaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.tauschring-waiblingen.de">tauschring-waiblingen.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Rheingold</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.rheingoldregio.de">rheingoldregio.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Roland</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.roland-regional.de">roland-regional.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">Steintaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.steintaler.org">steintaler.org</a></div>
+			</div>
+			
+			<!--<div class="project">
+			  <div class="project_name">Tauberfranken</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.tauber-franken-regiogeld.de">tauber-franken-regiogeld.de</a></div>
+			</div>-->
+			
+			<div class="project">
+			  <div class="project_name">Teutotaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.teutotaler.de">teutotaler.de</a></div>
+			</div>
+			
+			<div class="project">
+			  <div class="project_name">UrstromTaler</div>
+			  <div class="project_link"><a class="plink" target="_blank" href="http://www.urstromtaler.de">urstromtaler.de</a></div>
+			</div>
