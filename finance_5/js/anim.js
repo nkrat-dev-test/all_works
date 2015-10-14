0ba6f35721ea779ddd72477b2e9815efd8d708a9
my_new_git <boriskaborisenko@gmail.com>
2015-10-14 23:22:23 +0300

    re

--- finance_5/js/anim.js
+++ finance_5/js/anim.js
@@ -1,3 +1,10 @@
+
+ 
+ $('.looper').looper({
+    interval: 3500,
+    speed:1000
+});
+ 
  $("header").headroom({
    "offset": 205,
    "tolerance": 5,
@@ -48,12 +55,12 @@
 
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
-    $('#nav a').each(function () {
+    $('.nav a').each(function () {
 	   
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#nav ul li a').removeClass("active");
+            $('.nav ul li a').removeClass("active");
             currLink.addClass("active");
             
         }
@@ -67,14 +74,7 @@ function onScroll(event){
 
 
 
-$(".mobnav").on("tap",function(eventmenu1){
-        eventmenu1.stopPropagation();
-        $('.nav').toggleClass('slideInDown');
-}); 
 
-$('html').on("tap",function(){
-        $('.nav').toggleClass('slideInDown');
-}); 
       
       
     $('.mobnav').click( function(eventmenu2){
@@ -94,4 +94,247 @@ $('html').on("tap",function(){
         $('.nav').removeClass('slideDown2');
         $('.nav').removeClass('show');
         
+    }); 
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    var rmarker = $('#m1');
+
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        var z = $('#m1').offset().top;
+
+        if (y >= z) {
+	        
+setTimeout(function(){
+  $('.pin6').addClass('slideDown2');
+}, 200);	         
+
+
+
+setTimeout(function(){
+  $('.pin5').addClass('slideUp2');
+}, 400);
+setTimeout(function(){
+  $('.pin4').addClass('slideDown2');
+}, 400);
+setTimeout(function(){
+  $('.pin3').addClass('slideDown2');
+}, 800);
+setTimeout(function(){
+  $('.pin1').addClass('animated zoomInDown');
+  $('.pin2').addClass('animated zoomInDown');
+}, 1000);
+
+setTimeout(function(){
+   $('#t1').addClass('animated fadeInUp');
+}, 700); 
+       
+        
+        }
+        
+    });
+    });    
+     
+
+
+
+
+
+
+$(document).ready(function () {
+    var rmarker = $('#m2');
+
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        var z = $('#m2').offset().top;
+
+        if (y >= z) {
+
+setTimeout(function(){
+   $('.g1').addClass('animated fadeInUp');
+}, 300); 
+setTimeout(function(){
+   $('.g2').addClass('animated fadeInUp');
+}, 800);
+
+setTimeout(function(){
+   $('.g3').addClass('animated fadeInUp');
+}, 500); 
+	        
+setTimeout(function(){
+   $('.g4').addClass('animated fadeInUp');
+}, 1200); 
+
+setTimeout(function(){
+   $('.g5').addClass('animated fadeInLeft');
+}, 1700);
+
+setTimeout(function(){
+   $('#t2').addClass('animated fadeInUp');
+}, 700); 
+       
+        
+        }
+        
+    });
+    });    
+    
+    
+    
+    
+    
+    
+  $(document).ready(function () {
+    var rmarker = $('#m3');
+
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        var z = $('#m3').offset().top;
+
+        if (y >= z) {
+
+setTimeout(function(){
+   $('#t3').addClass('animated fadeInUp');
+}, 300); 
+
+       
+        
+        }
+        
+    });
+    });      
+     
+     
+     
+     
+     
+     $(document).ready(function () {
+    var rmarker = $('#m4');
+
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        var z = $('#m4').offset().top;
+
+        if (y >= z) {
+
+setTimeout(function(){
+   $('#t5').addClass('animated flipInY');
+}, 300); 
+
+setTimeout(function(){
+   $('#t6').addClass('animated flipInY');
+}, 500);
+
+setTimeout(function(){
+   $('#t7').addClass('animated flipInY');
+}, 800);
+
+setTimeout(function(){
+   $('#t8').addClass('animated flipInY');
+}, 1100);
+
+       
+        
+        }
+        
+    });
+    });      
+     
+     
+     
+     
+     
+      $(document).ready(function () {
+    var rmarker = $('#m5');
+
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        var z = $('#m5').offset().top;
+
+        if (y >= z) {
+
+setTimeout(function(){
+   $('#t9').addClass('animated fadeInUp');
+}, 300); 
+
+setTimeout(function(){
+   $('#t10').addClass('animated fadeInUp');
+}, 500);
+
+setTimeout(function(){
+   $('#t11').addClass('animated flipInY');
+}, 800);
+
+       
+        
+        }
+        
+    });
+    });      
+    
+    
+      $(document).ready(function () {
+    var rmarker = $('#m6');
+
+    $(window).scroll(function () {
+        var y = $(this).scrollTop();
+        var z = $('#m6').offset().top;
+
+        if (y >= z) {
+
+setTimeout(function(){
+   $('#t12').addClass('animated zoomInUp');
+}, 300); 
+
+setTimeout(function(){
+   $('#t13').addClass('animated zoomInUp');
+}, 500);
+
+setTimeout(function(){
+   $('#t14').addClass('animated zoomInUp');
+}, 800);
+
+       
+        
+        }
+        
+    });
     });  
+    
+    
+  $("#send").prop('disabled', true);   
+  function check() {
+  //var phone = $.trim($("#ph").val());
+  var email = $.trim($("#em").val());
+  var subj = $.trim($("#sb").val());
+  
+  if( email.length && subj.length >0) {
+    $("#send").prop('disabled', false);
+    $("#send").removeClass('disable');
+  } else {
+    $("#send").prop('disabled', true);
+    $("#send").addClass('disable');
+  }
+}    
+     
