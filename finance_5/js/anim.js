2fe7236c711a5a41c25b20fed57d9a05a03351f8
my_new_git <boriskaborisenko@gmail.com>
2015-10-14 13:10:40 +0300

    finance 5 responsive test

---
+++ finance_5/js/anim.js
@@ -0,0 +1,65 @@
+ $("header").headroom({
+   "offset": 205,
+   "tolerance": 5,
+   "classes": {
+     "initial": "animated",
+     "pinned": "slideInDown",
+     "unpinned": "slideOutUp"
+   }
+ });
+ 
+ // to destroy
+ $("#header").headroom("destroy");
+ 
+ //pin
+ 
+ 
+ 
+ 
+	 $(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+       
+        
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+      
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+0
+        }, 500, 'swing', function () {
+            window.location.hash = '';
+            $(document).on("scroll", onScroll);
+        
+        
+        });
+        
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#nav a').each(function () {
+	   
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#nav ul li a').removeClass("active");
+            currLink.addClass("active");
+            
+        }
+        else{
+            currLink.removeClass("active");
+            
+        }
+    });
+}  
