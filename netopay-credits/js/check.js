d441496f2b4274ee07b7a1e97d91428d715f04d7
my_new_git <boriskaborisenko@gmail.com>
2015-10-19 17:46:54 +0300

    without scheme

--- netopay-credits/js/check.js
+++ netopay-credits/js/check.js
@@ -1,5 +1,5 @@
 $(document).ready(function() { 
-            $('#cform').ajaxForm(function() { 
+            $('#cform').ajaxForm(function() {
 	            $("#em").val('');
 	            $("#sb").val('');
 	            $("#feed").val('');
