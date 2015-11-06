2b290d05285369d2729a1040889df72230507315
my_new_git <boriskaborisenko@gmail.com>
2015-11-06 10:54:05 +0200

    a few changes

---
+++ rel6/en/table.php
@@ -0,0 +1,79 @@
+<div class="for_tables">
+				<div class="one_table"><table>
+			        <tr>
+				        <th colspan="3">The contribution rates established in the region</th>
+				    </tr>
+				    <tr>
+					    <td colspan="2">Social contributions</td>
+					    <td class="center">750,00</td>
+				    </tr>
+				     <tr>
+					    <td colspan="2">Transactional payments</td>
+					    <td class="center">250,00</td>
+				    </tr>
+				     <tr>
+					    <td colspan="2">The amount of deductions, Quant.<span class="regio">Regio</span></td>
+					    <td class="center"><span>– 1000,00</span></td>
+				    </tr>
+				    <tr>
+					    <td class="sepx" colspan="3">The amount of deductions distributed, in the equivalent of values (EV)</td>
+					</tr>
+					 <tr>
+					    <td><span>— For infrastructure</span></td>
+					    <td class="center">10%</td>
+					    <td class="center"><span>–</span> 100</td>
+				    </tr>
+				    <tr>
+					    <td><span>— For medicine</span></td>
+					    <td class="center">10%</td>
+					    <td class="center"><span>–</span> 100</td>
+				    </tr>
+				    <tr>
+					    <td><span>— For education</span></td>
+					    <td class="center">25%</td>
+					    <td class="center"><span>–</span> 250</td>
+				    </tr>
+				    <tr>
+					    <td><span>— For the environment</span></td>
+					    <td class="center">5%</td>
+					    <td class="center"><span>–</span> 50</td>
+				    </tr>
+				    <tr>
+					    <td><span>— For safety</span></td>
+					    <td class="center">25%</td>
+					    <td class="center"><span>–</span> 250</td>
+				    </tr>
+				    <tr>
+					    <td><span>— For social security</span></td>
+					    <td class="center">25%</td>
+					    <td class="center"><span>–</span> 250</td>
+				    </tr>
+		        </table></div>
+
+				<div class="one_table"><table>
+			     <tr>
+				     <th colspan="2">Fulfilled rules by user</th>
+			     </tr>
+			     <tr>
+				     <td>Maintains a healthy lifestyle, in Quant.<span>Medical</span></td>
+				     <td class="center gr"><span>+</span> 90,00</td>
+				 </tr> 
+				  <tr>
+				     <td>Attends educational courses: children get high grades in school (the University), in Quant.<span>Education</span></td>
+				     <td class="center gr"><span>+</span> 200,00</td>
+				 </tr> 
+				  <tr>
+				     <td>Uses environmentally friendly car, in Quant.<span>Ecology</span></td>
+				     <td class="center gr"><span>+</span> 45,00</td>
+				 </tr>   
+				  <tr>
+				     <td>The house has security and fire alarm systems, in Quant.<span>Social</span></td>
+				     <td class="center gr"><span>+</span> 125,00</td>
+				 </tr> 
+				  <tr>
+				     <td class="summary">The amount to be reimbursed, in Quant.<span class="regio">Regiо</span></td>
+				     <td class="center gr"><span>+ 460,00</span></td>
+				 </tr>   
+			    </table></div>
+</div>
+
